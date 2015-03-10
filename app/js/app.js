21271560372165b4b4c38287f5fdb522379b6933
Hayden Ball <hayden@haydenball.me.uk>
2015-03-10 22:47:54 +0000

    Added animation to new tweet

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['appControllers', 'appServices', 'appDirectives', 'appFilters']);
+var app = angular.module('app', ['ngAnimate', 'appControllers', 'appServices', 'appDirectives', 'appFilters']);
 var appControllers = angular.module('appControllers', []);
 var appServices = angular.module('appServices', []);
 var appDirectives = angular.module('appDirectives', []);
